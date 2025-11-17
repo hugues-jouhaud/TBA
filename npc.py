f1a7916f63ba0accfcfbf83aa7a1554668c5049b
hugues-jouhaud <hugues.jouhaud@edu.esiee.fr>
2025-11-17 18:47:41 +0100

    - Correction : Passage de npc en UTF8

--- npc.py
+++ npc.py
@@ -3,8 +3,8 @@ import random
 
 class Monstre:
     """
-    Représente un monstre (NPC) qui se déplace dans le jeu.
-    Il peut se dépalcer et calculer sa distance au joueur.
+    ReprÃ©sente un monstre (NPC) qui se dÃ©place dans le jeu.
+    Il peut se dÃ©palcer et calculer sa distance au joueur.
     """
     
     # Constructeur
@@ -23,7 +23,7 @@ class Monstre:
             player_room (Room): La salle actuelle du joueur.
 
         Returns:
-            int: La distance (0 si même salle, -1 si non trouvé/invalide).
+            int: La distance (0 si mÃªme salle, -1 si non trouvÃ©/invalide).
         """
         
         salle_de_depart = self.current_room
@@ -33,13 +33,13 @@ class Monstre:
         if not salle_de_depart or not salle_cible:
             return -1
 
-        # S'ils sont dans la même pièce
+        # S'ils sont dans la mÃªme piÃ¨ce
         if salle_de_depart == salle_cible:
             return 0
 
         # --- Initialisation de "l'exploration" ---
         #    On stocke des tuples (salle, distance_actuelle)
-        #    On commence par la salle de départ (distance 0).
+        #    On commence par la salle de dÃ©part (distance 0).
         file_a_visiter = [(salle_de_depart, 0)]
         
         salles_visitees = {salle_de_depart}
@@ -47,13 +47,13 @@ class Monstre:
         
         while file_a_visiter:
             
-            # On prend la première salle de notre liste
+            # On prend la premiÃ¨re salle de notre liste
             (salle_actuelle, distance_actuelle) = file_a_visiter.pop(0)
 
             for prochaine_salle in salle_actuelle.exits.values():
                 
                 # Si c'est une vraie sortie (pas 'None')
-                # ET qu'on ne l'a pas encore visitée
+                # ET qu'on ne l'a pas encore visitÃ©e
                 if prochaine_salle and prochaine_salle not in salles_visitees:
                     # C'est la salle du joueur ?
                     if prochaine_salle == salle_cible:
@@ -62,16 +62,16 @@ class Monstre:
 
                     # Sinon :
                     
-                    # 1. On la marque comme visitée et note la nouvelle distance
+                    # 1. On la marque comme visitÃ©e et note la nouvelle distance
                     salles_visitees.add(prochaine_salle)
                     file_a_visiter.append((prochaine_salle, distance_actuelle + 1))
-        # Pas trouvé ...
+        # Pas trouvÃ© ...
         return -1
 
     def move(self):
         """
-        Déplace le monstre d'une salle au hasard SI il a spawn.
-        Choisit une sortie valide et met à jour self.current_room.
+        DÃ©place le monstre d'une salle au hasard SI il a spawn.
+        Choisit une sortie valide et met Ã  jour self.current_room.
         """
         
         # 0. Le monstre a t'il spawn ?
@@ -84,6 +84,6 @@ class Monstre:
             if salle is not None
         ]
 
-        # 2. Choisir une salle aléatoire et déplacé le monstre dedans.
+        # 2. Choisir une salle alÃ©atoire et dÃ©placÃ© le monstre dedans.
         nouvelle_salle = random.choice(sorties_valides)
         self.current_room = nouvelle_salle
